e4917e9fa1f5d6137e8fc033e3df770603be611a
Soumodipta Bose <sbose2019@gmail.com>
2022-03-20 10:40:23 +0530

    logging kibana dashboard config needed

--- logging/logger_main.py
+++ logging/logger_main.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 import json
 consumer = KafkaConsumer('logging', group_id='g1',
-                         bootstrap_servers=['localhost:9092'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+                         bootstrap_servers=['localhost:9094'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 for message in consumer:
     print(message.value)
--- logging/test_producer.py
+++ logging/test_producer.py
@@ -4,7 +4,7 @@ from platform_logger import get_logger
 import logging
 from faker import Faker
 fake = Faker()
-logger = get_logger('sensor_manager', 'localhost:9092')
+logger = get_logger('sensor_manager', 'localhost:9094')
 while True:
     logger.info(fake.text())
     sleep(1)
