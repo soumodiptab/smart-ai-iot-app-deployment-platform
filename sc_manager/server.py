f2f2da8e1d515343fe80a2db255c8850d594b2d8
sourav7rajput <sourav7rajput@gmail.com>
2022-03-31 17:09:21 +0530

    sc_type upload/display

--- sc_manager/server.py
+++ sc_manager/server.py
@@ -1,15 +1,18 @@
-
+from asyncio import tasks
 from flask import Flask, flash, redirect, render_template, request, jsonify, url_for
 from werkzeug.utils import secure_filename
 from sc_db_interaction import validate_sc_type_and_insert, validator_sc_instance_and_insert
 import json
+from bson.json_util import dumps
+from pymongo import MongoClient
 import os
+import logging
 import shutil
 from utils import allowed_file_extension
 ALLOWED_EXTENSIONS = {'zip', 'rar'}
 UPLOAD_FOLDER = 'temp'
 PORT = 8101
-
+log=logging.getLogger('demo-logger')
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -75,7 +78,26 @@ def sc_instance_upload():
 
 @app.route('/sc_type/display', methods=['POST', 'GET'])
 def sc_type_display():
-    return jsonify({'status': '200'})
+    try:
+        MONGO_DB_URL = "mongodb://localhost:27017/"
+        client = MongoClient(MONGO_DB_URL)
+        db = client.sc_db
+        sc_type_list=[]
+        Project_List_Col = db.sc_type
+        for sc_type_record in list(Project_List_Col.find()):
+            display_record={
+                "company":sc_type_record["company"],
+                "model": sc_type_record["model"],
+                "parameter_count":sc_type_record["parameter_count"],
+                "parameters":sc_type_record["parameters"],
+                "device":sc_type_record["device"],
+                "type":sc_type_record["type"]
+            }
+            sc_type_list.append(display_record)
+            print(sc_type_list)
+        return render_template('display.html',tasks=sc_type_list)
+    except Exception as e:
+        log.error({'error': str(e)})
 
 
 if __name__ == '__main__':
