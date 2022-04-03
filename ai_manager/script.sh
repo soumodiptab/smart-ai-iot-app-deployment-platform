33a3c761a67f32bb29e42bc92dca4f558b95ef2d
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-04-03 15:02:13 +0530

    revamping the script.sh

--- ai_manager/script.sh
+++ ai_manager/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 kill -9 $(ps aux|grep '[p]ython3 server.py' | awk ' { print $2 } ')
 python3 server.py $1 &
