a30fb3591f556dbf2654346f70a619637de08303
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-04-05 14:37:23 +0530

    0.0.0.0

--- ai_manager/server.py
+++ ai_manager/server.py
@@ -138,8 +138,8 @@ def model_display():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0",port=PORT, debug=True, use_debugger=False,
-    #         use_reloader=False, passthrough_errors=True)
-    
-    app.run(port=PORT, debug=True, use_debugger=False,
+    app.run(host="0.0.0.0",port=PORT, debug=True, use_debugger=False,
             use_reloader=False, passthrough_errors=True)
+    
+    # app.run(port=PORT, debug=True, use_debugger=False,
+    #         use_reloader=False, passthrough_errors=True)
