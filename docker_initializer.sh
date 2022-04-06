b8d010157446e700287331565ed82e9e536cbe29
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-04-07 03:32:08 +0530

    added docker initializer

---
+++ docker_initializer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Go to the repo
+cd repo/smart-ai-iot-app-deployment-platform
+
+# pull the latest repo
+git pull
+
+# Remove all dangling docker images
+sudo docker image prune
+
+# Docker run for ai_manager
+cd ai_manager
+sudo docker build -t ai_manager:latest . 
+sudo docker run -d --rm -p 6500:6500 ai_manager
+cd ..
+
+# Docker run for app_manager
+cd app_manager
+sudo docker build -t app_manager:latest . 
+sudo docker run -d --rm -p 8200:8200 app_manager
+cd ..
+
+# Docker run for request_manager
+cd request_manager
+sudo docker build -t request_manager:latest . 
+sudo docker run --rm -p 8080:8080 request_manager
+cd ..
+
+# Docker run for sc_manager
+cd sc_manager
+sudo docker build -t sc_manager:latest . 
+sudo docker run --rm -p 8101:8101 sc_manager
+cd ..
+
+# Docker run for sensor_data_interface
+cd sensor_data_interface
+sudo docker build -t simulator:latest . 
+sudo docker run -d --rm simulator
+cd ..
+
+# Docker run for test_model
+cd ~/test/model
+sudo docker run --rm -p 9050:9050 testmodel 9050
+cd ~
