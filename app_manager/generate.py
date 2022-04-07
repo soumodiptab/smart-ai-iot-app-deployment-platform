17806ed79ab3887a0c4a0aa181091d1b0ab2dfe6
Soumodipta Bose <sbose2019@gmail.com>
2022-04-07 20:20:58 +0530

    docker file bug fix

--- app_manager/generate.py
+++ app_manager/generate.py
@@ -3,18 +3,19 @@ from re import L
 import sys
 from zipfile import ZipFile
 
+
 def generateDockerFile(path):
-    extract_path=" "
+    extract_path = " "
     with ZipFile(path, 'r') as zip:
         path = path[:-4]
         print(path)
         zip.extractall(path)
-    
+
     print(path)
-    path1=path+"/config/control.json"
-    f=open(path1)
-    data=json.load(f)
-    file_name=data['main']
+    path1 = path+"/config/control.json"
+    f = open(path1)
+    data = json.load(f)
+    file_name = data['main']
     print(file_name)
     fname = path + '/Dockerfile'
     wdata = "FROM python:3.8-slim-buster\n\n"
@@ -22,7 +23,7 @@ def generateDockerFile(path):
     wdata += "COPY requirements.txt requirements.txt\n\n"
     wdata += "RUN pip3 install -r requirements.txt\n\n"
     wdata += "COPY . .\n\n"
-    wdata += "ENTRYPOINT [ 'python3' ]\n\n"
+    wdata += "ENTRYPOINT [ \"python3\" ]\n\n"
     wdata += f'CMD ["{file_name}"]\n\n'
     with open(fname, 'w') as f:
         f.write('{}'.format(wdata))
