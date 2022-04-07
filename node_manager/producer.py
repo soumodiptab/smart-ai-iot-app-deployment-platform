76f4f346f0a2d5880cbb585ba2cb02b23bfe3db0
vishpandey <vishpandey2012@gmail.com>
2022-04-07 17:28:28 +0530

    changing appstrat script

--- node_manager/producer.py
+++ node_manager/producer.py
@@ -7,7 +7,8 @@ deploy_producer = KafkaProducer(bootstrap_servers=["13.71.109.62:9092"],
                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 
-deploy_producer.send("deploy_127.0.0.1", "hello asshole")
+req = {"message_type": "SCHED_APP", "app_id": "64af94339aba44e3a86504039cc85e26", "isModel": False, "app_instance_id": "63505624f61245d38ede707b3d805343", "start_time": "03:30", "end_time": "05:35", "periodicity": "1", "burst_time": "1", "periodicity_unit": "Hrs"}
+deploy_producer.send("scheduler", req)
 time.sleep(2)
 
 
