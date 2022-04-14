0e08e13a6f313b5f357990dfbf36cae38f239ca8
Ubuntu <sudipta@VM3.jogh0xwjjteengbku1pjexwc4g.tx.internal.cloudapp.net>
2022-04-14 06:47:37 +0530

    resolve deployment issues

--- node_manager/initialiser/initialiser.py
+++ node_manager/initialiser/initialiser.py
@@ -31,7 +31,7 @@ collection=app_info[collection_name]
 
 @app.route('/initialiser/getDeploymentNodes', methods=['GET'])
 def getDeploymentNodes():
-    cursor = collection.find({"name": "node-agent"})
+    cursor = collection.find({"type": "node-agent"})
     node_agent_addresses = []
     for doc in cursor:
         node_agent_address = {}
