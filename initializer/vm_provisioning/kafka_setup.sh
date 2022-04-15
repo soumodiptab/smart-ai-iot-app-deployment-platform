635b104cdf3dd87416ad15ee0788ab9082b593ac
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-04-15 12:40:25 +0530

    upadted kafka script

--- initializer/vm_provisioning/kafka_setup.sh
+++ initializer/vm_provisioning/kafka_setup.sh
@@ -30,7 +30,8 @@ for vm_name in "${VM_NAMES[@]}"
 do
 PUBLIC_IP_ADDRESS=$(az vm create --resource-group $RESOURCE_GROUP_NAME \
   --name $vm_name \
-  --image UbuntuLTS \
+  --image Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest \
+  --admin-username azureuser \
   --output json \
   --verbose \
   --public-ip-sku Standard \
@@ -42,12 +43,7 @@ PUBLIC_IP_ADDRESS=$(az vm create --resource-group $RESOURCE_GROUP_NAME \
 
 VM_PUBLIC_IPs+=($PUBLIC_IP_ADDRESS)
 az vm open-port --port 80 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 600
-az vm open-port --port 2376 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 700
 az vm open-port --port 9092 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 800
-az vm open-port --port 27017 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 1000
-az vm open-port --port 8004 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 1200
-az vm open-port --port 10000 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 1100
-az vm open-port --port 50000 --resource-group $RESOURCE_GROUP_NAME --name $vm_name --priority 1300
 done
 
 printf "\n\n"
@@ -64,9 +60,9 @@ do
   UN_NEW="${UN_NEW#\'}"
   echo $IP_NEW
   echo $UN_NEW
-  sshpass -f pass ssh -o StrictHostKeyChecking=no $UN_NEW@$IP_NEW "sudo apt-get install sshpass; sudo apt install -y python3-pip; sudo -H pip3 install --upgrade pip;"
+  sshpass -f pass ssh -o StrictHostKeyChecking=no $UN_NEW@$IP_NEW "sudo apt-get install sshpass; sudo apt install -y python3-pip; sudo -H pip3 install --upgrade pip; sudo apt-get -y install python3.8;"
   
-#   sshpass -f pass ssh -o StrictHostKeyChecking=no $UN_NEW@$IP_NEW "sudo apt install curl; curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; sudo apt-get install sshpass; sudo apt install -y python3-pip;sudo -H pip3 install --upgrade pip; chmod +x ./mongodb_install.sh; ./mongodb_install.sh;"
+  # sshpass -f pass ssh -o StrictHostKeyChecking=no $UN_NEW@$IP_NEW "sudo apt install curl; curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; sudo apt-get install sshpass; sudo apt install -y python3-pip;sudo -H pip3 install --upgrade pip; chmod +x ./mongodb_install.sh; ./mongodb_install.sh;"
   # sshpass -f pass scp -o StrictHostKeyChecking=no -r node $UN_NEW@$IP_NEW:node
   # sshpass -f pass ssh -o StrictHostKeyChecking=no $UN_NEW@$IP_NEW "cd node && python3 node2.py" &
 done
