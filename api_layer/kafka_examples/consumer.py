34eebe74e30f992123662a2480fe09bcffe55096
akileshpanicker <48186557+akileshpanicker@users.noreply.github.com>
2022-04-22 10:35:08 +0530

    Delete api_layer directory

--- api_layer/kafka_examples/consumer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-import json
-consumer = KafkaConsumer('new-route',
-                         bootstrap_servers=['localhost:9094'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-for message in consumer:
-    msg = message.value
-    print(msg)
--- api_layer/sc_tests/dir_test.py
+++
@@ -1,4 +0,0 @@
-import os
-direc = "images"
-path = os.path.join(direc, 'asdasd')
-os.makedirs(path)
