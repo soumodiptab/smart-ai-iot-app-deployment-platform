e4310d696f1e35ec66bb15569e8a48952a9c0e83
akileshpanicker <48186557+akileshpanicker@users.noreply.github.com>
2022-04-22 11:11:25 +0530

    Add files via upload

---
+++ packaging_details/Final Test Case/application/scripts/script1.py
@@ -0,0 +1,82 @@
+from random import randint
+from platform_sdk import get_prediction, get_sensor_data, send_controller_data, get_sensor_image
+import time
+from pymongo import MongoClient
+from platform_sdk import get_mongo_db_uri
+from logging import Logger
+import logging
+log = logging.getLogger('demo-logger')
+MONGO_DB_URL = get_mongo_db_uri()
+#MONGO_DB_URL = "mongodb://localhost:27017/"
+client = MongoClient(MONGO_DB_URL)
+
+MONGO_DB_URL = get_mongo_db_uri()
+# client = MongoClient(MONGO_DB_URL)
+# db = client.model_stat
+# db.instance.insert_one({"modelId": "0", "class_0": 0,
+#                         "class_1": 0, "class_2": 0, "class_3": 0, "class_4": 0})
+
+
+def script_function():
+    db = client.model_stat
+
+    c0 = list(db.instance.find({"modelId": "0"}))[0]['class_0']
+    c1 = list(db.instance.find({"modelId": "0"}))[0]['class_1']
+    c2 = list(db.instance.find({"modelId": "0"}))[0]['class_2']
+    c3 = list(db.instance.find({"modelId": "0"}))[0]['class_3']
+    c4 = list(db.instance.find({"modelId": "0"}))[0]['class_4']
+    j = 0
+    d = {'Drizzle': 0, 'Fog': 1, 'Rain': 2, 'Snow': 3, 'Sun': 4}
+
+    while True:
+        temp = get_sensor_data(0)
+        # temp = 1
+        pres = get_sensor_data(1)
+        # pres = 2
+        # val = get_prediction(0, {
+        #     "temp_max": temp,
+        #     "perc": pres
+        # })['output']
+        val = 'Sun'
+        print("Thread Working")
+        if d[val] == 0:
+            c0 += 1
+            try:
+                db.instance.update_one(
+                    {"modelId": "0"}, {"$set": {"class_0": c0}})
+            except Exception as e:
+                log.error({'error': str(e)})
+
+        elif d[val] == 1:
+            c1 += 1
+            try:
+                db.instance.update_one(
+                    {"modelId": "0"}, {"$set": {"class_1": c1}})
+            except Exception as e:
+                log.error({'error': str(e)})
+        elif d[val] == 2:
+            c2 += 1
+            try:
+                db.instance.update_one(
+                    {"modelId": "0"}, {"$set": {"class_2": c2}})
+            except Exception as e:
+                log.error({'error': str(e)})
+        elif d[val] == 3:
+            c3 += 1
+            try:
+                db.instance.update_one(
+                    {"modelId": "0"}, {"$set": {"class_3": c3}})
+            except Exception as e:
+                log.error({'error': str(e)})
+        elif d[val] == 4:
+            c4 += 1
+            try:
+                db.instance.update_one(
+                    {"modelId": "0"}, {"$set": {"class_4": c4}})
+            except Exception as e:
+                log.error({'error': str(e)})
+        # j += 1
+        send_controller_data(0, val)
+        time.sleep(3)
+        # if (j == 100):
+        #     break
