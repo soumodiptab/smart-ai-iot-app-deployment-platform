85d2eb34f6e50da412456fd35a1653c02874f15e
Soumodipta Bose <sbose2019@gmail.com>
2022-04-23 00:48:27 +0530

    Proxy built for AI

--- node_manager/node-agent/node_agent.py
+++ node_manager/node-agent/node_agent.py
@@ -42,12 +42,14 @@ app_info = client[database_name]
 collection_name = cfg["mongo"]["collection"]
 collection = app_info[collection_name]
 
+
 def getSelfIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     self_ip = s.getsockname()[0]
     s.close()
     return self_ip
+
 
 def startAppDeployment(deployment_info):
     if "isModel" not in deployment_info:
@@ -60,13 +62,15 @@ def startAppDeployment(deployment_info):
     free_port = find_free_port()
     self_ip = getSelfIp()
     print("here", app_id, app_instance_id, isModel)
-    
+
     print(type(isModel))
     print(isModel)
     if isModel == "1":
-        getAppZipFromStorage(app_id, "aibucket", app_instance_id, self_ip, free_port, isModel)
+        getAppZipFromStorage(app_id, "aibucket",
+                             app_instance_id, self_ip, free_port, isModel)
     else:
-        getAppZipFromStorage(app_id, "appbucket", app_instance_id, self_ip, free_port, isModel)
+        getAppZipFromStorage(app_id, "appbucket",
+                             app_instance_id, self_ip, free_port, isModel)
 
     # updateAppConfig(app_instance_id, self_ip, free_port)
 
@@ -75,7 +79,8 @@ def startAppDeployment(deployment_info):
 
 
 def updateAppConfig(app_instance_id, ip, free_port):
-    app_instance_id_dir = os.environ.get("NODE_AGENT_HOME") + "/" + app_instance_id
+    app_instance_id_dir = os.environ.get(
+        "NODE_AGENT_HOME") + "/" + app_instance_id
     config_directory = app_instance_id_dir + "/config"
 
     isExists = os.path.exists(config_directory)
@@ -84,34 +89,38 @@ def updateAppConfig(app_instance_id, ip, free_port):
         os.makedirs(config_directory)
 
     app = {}
-    app['app_instance_id']=app_instance_id
+    app['app_instance_id'] = app_instance_id
 
     print(config_directory)
 
-    with open(config_directory +'/app.json', 'w') as f:
+    with open(config_directory + '/app.json', 'w') as f:
         json.dump(app, f)
 
     db = {}
     db['DATABASE_URI'] = cfg["mongo"]["address"]
 
     print("creating db file")
-    with open(config_directory + '/db.json','w')as f:
+    with open(config_directory + '/db.json', 'w')as f:
         json.dump(db, f)
 
     kafka1 = {}
-    kafka1['bootstrap_servers']= cfg["kafka"]["servers"]
+    kafka1['bootstrap_servers'] = cfg["kafka"]["servers"]
 
-    with open(config_directory + '/kafka.json','w')as f:
+    with open(config_directory + '/kafka.json', 'w')as f:
         json.dump(kafka1, f)
 
     download_blob("deploymentbucket", "platform_sdk.py", app_instance_id)
 
+
 def download_blob(bucket, file_path, app_instance_id):
-    sdk_file_path = os.environ.get("NODE_AGENT_HOME") + "/" + app_instance_id + "/" + file_path
-    service = ShareFileClient.from_connection_string(conn_str="https://iasprojectaccount.file.core.windows.net/DefaultEndpointsProtocol=https;AccountName=iasprojectaccount;AccountKey=3m7pA/FPcLIe195UhnJ7bZUMueN8FBPBpKUF42lsEP9xk3ZWzM3XpeSh4NWq+cOOitaLmJbU7hJ2UWLdrVL8NQ==;EndpointSuffix=core.windows.net", share_name=bucket, file_path=file_path)
+    sdk_file_path = os.environ.get(
+        "NODE_AGENT_HOME") + "/" + app_instance_id + "/" + file_path
+    service = ShareFileClient.from_connection_string(
+        conn_str="https://iasprojectaccount.file.core.windows.net/DefaultEndpointsProtocol=https;AccountName=iasprojectaccount;AccountKey=3m7pA/FPcLIe195UhnJ7bZUMueN8FBPBpKUF42lsEP9xk3ZWzM3XpeSh4NWq+cOOitaLmJbU7hJ2UWLdrVL8NQ==;EndpointSuffix=core.windows.net", share_name=bucket, file_path=file_path)
     with open(sdk_file_path, "wb") as file_handle:
         data = service.download_file()
         data.readinto(file_handle)
+
 
 def find_free_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
@@ -127,6 +136,7 @@ def getNodeStats():
     RAM_use = psutil.virtual_memory()[2]
     to_send = {"CPU": str(CPU_use), "RAM": str(RAM_use), "Status": "1"}
     return to_send
+
 
 def updateNodeDeploymentStatus(app_id, app_instance_id, ip, port, status):
     query = {"app_instance_id": app_instance_id}
@@ -176,19 +186,18 @@ def getAppZipFromStorage(app_id, bucket_name, app_instance_id, self_ip, free_por
     except Exception as e:
         print(e)
 
-    unzip_run_app(zip_file_name, app_id, app_instance_id, self_ip, free_port, isModel)
-
+    unzip_run_app(zip_file_name, app_id, app_instance_id,
+                  self_ip, free_port, isModel)
 
 
 def unzip_run_app(app_zip_file, app_id, app_instance_id, self_ip, free_port, isModel):
     app_zip_full_path = os.environ.get("NODE_AGENT_HOME") + "/" + app_zip_file
     print(app_zip_full_path)
-
     dest_path = os.environ.get("NODE_AGENT_HOME") + "/" + app_instance_id
     with zipfile.ZipFile(app_zip_full_path, "r") as zipobj:
         zipobj.extractall(dest_path)
-
-    dest_path_after_rename = os.environ.get("NODE_AGENT_HOME") + "/" + app_instance_id
+    dest_path_after_rename = os.environ.get(
+        "NODE_AGENT_HOME") + "/" + app_instance_id
     # os.rename(dest_path, dest_path_after_rename)
     #os.system("mv " + dest_path + " " + dest_path_after_rename)
     if isModel == "0":
@@ -196,34 +205,36 @@ def unzip_run_app(app_zip_file, app_id, app_instance_id, self_ip, free_port, isM
 
     # try:
     req_file_path = dest_path_after_rename + "/requirements.txt"
-        # req_installation_data = subprocess.Popen(
-        #     ['pip', 'install', '-r', req_file_path], stdout=subprocess.PIPE)
-        # req_installation_output = req_installation_data.communicate()
+    # req_installation_data = subprocess.Popen(
+    #     ['pip', 'install', '-r', req_file_path], stdout=subprocess.PIPE)
+    # req_installation_output = req_installation_data.communicate()
 
-        # os.system("pip install -r " +  req_file_path)
-        # os.chdir(app_instance_id)
-        # os.system("python3 " + dest_path_after_rename + "/app.py &")
+    # os.system("pip install -r " +  req_file_path)
+    # os.chdir(app_instance_id)
+    # os.system("python3 " + dest_path_after_rename + "/app.py &")
 
-        # os.chdir('config')
+    # os.chdir('config')
 
-        # data = json.load('config/control.json')
-        # for i in data['scripts']:
-        #     os.system("python3" + i['filename'] + " " + i['args'] + "&")
-    os.chdir(dest_path_after_rename)
-    #print(os.getcwd())
+    # data = json.load('config/control.json')
+    # for i in data['scripts']:
+    #     os.system("python3" + i['filename'] + " " + i['args'] + "&")
+    # os.chdir(dest_path_after_rename)
+    # print(os.getcwd())
     #os.system("sudo docker build -t sample_app:latest .")
     print(free_port)
     #os.system("sudo docker run --rm -p 6015:6015 sample_app")
     #client = docker.from_env()
     #client.containers.run("ubuntu:latest", "sleep infinity", detach=True)
 
-    docker_image = docker.build('.', tags=app_instance_id)
+    docker_image = docker.build(dest_path_after_rename, tags=app_instance_id)
     docker.run(app_instance_id, detach=True, publish=[(free_port, 6015)])
 
 
 def getSelfIp():
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    external_ip = urllib.request.urlopen(
+        'https://ident.me').read().decode('utf8')
     return external_ip
 
+
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5001)
--- request_manager/server.py
+++ request_manager/server.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, session, request, redirect, url_for, flash
-from logging import Logger
-import logging
 import sys
 from pymongo import MongoClient
 from utils import json_config_loader
-
+from platform_logger import get_logger
 import requests
 
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-# mydb = myclient["user_db"]  # database_name
-# mycol = mydb["users"]  # collection_name
+INITIALIZER_ADDRESS = json_config_loader('config/initialiser.json')["ADDRESS"]
+KAFKA_SERVERS = json_config_loader('config/kafka.json')["bootstrap_servers"]
+log = get_logger('request_manager', KAFKA_SERVERS)
+
+
+def getServiceUrl(service_name):
+    URL = "http://" + INITIALIZER_ADDRESS + \
+        "/initialiser/getService/" + service_name
+    r = requests.get(url=URL)
+    data = r.json()
+    ip = data["ip"]
+    port = data["port"]
+    url = "http://" + ip + ":" + port
+    return url
+
 
 MONGO_DB_URL = json_config_loader('config/db.json')['DATABASE_URI']
 client = MongoClient(MONGO_DB_URL)
@@ -23,8 +33,6 @@ PORT = sys.argv[1]
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-
-log = logging.getLogger('demo-logger')
 # MONGO_DB_URL = "mongodb://localhost:27017/"
 # client = MongoClient(MONGO_DB_URL)
 # db_user=client['users']
@@ -136,9 +144,7 @@ def home():
             port = ai_ip["port"]
             url3 = url3 + ip + ":" + port
         elif(user_role == 'Data Scientist'):
-            ip = ai_ip["ip"]
-            port = ai_ip["port"]
-            url = url + ip + ":" + port
+            return render_template("home.html", role=user_role)
         elif(user_role == 'Platform Configurer'):
             ip = sc_ip["ip"]
             port = sc_ip["port"]
@@ -249,12 +255,67 @@ def link_redirect():
             d_url += "/show_details"
 
             a = requests.get(d_url).content
-
             return a
-
         except:
             flash("App Instance Not Live", "error")
             return redirect(url_for('app_instance_display'))
+
+
+""" AI Manager """
+
+
+@app.route('/model/display', methods=['POST', 'GET'])
+def model_display():
+    # role_check = list(mycol.find({"username": session['user']}))
+    # user_role = role_check[0]['role']
+
+    # url = generate_ai_url(user_role)
+    # url = "http://127.0.0.1:6500/model/display"
+
+    # username = {"username": session['user']}
+    # model_details = requests.get(url, params=username).content
+
+    url = getServiceUrl("ai_manager") + "/model/display"
+    print(url)
+
+    model_details = requests.get(url).content
+    return model_details
+
+
+@app.route('/model/upload', methods=['POST', 'GET'])
+def model_upload():
+    if request.method == "GET":
+        # url = "http://127.0.0.1:6500/model/upload"
+
+        # username = {"username": session['user']}
+
+        # a = requests.get(url, params=username).content
+
+        # return a
+        url = getServiceUrl("ai_manager") + "/model/upload"
+        print(url)
+
+        model_upload_screen = requests.get(url).content
+        return model_upload_screen
+
+    else:
+        url = getServiceUrl("ai_manager") + "/model/upload"
+
+        # request.files["file"]
+
+        # files = {"file" : request.files}
+
+        file = request.files['file']
+
+        end_screen = requests.post(url, files={'file': (
+            file.filename, file.stream, file.content_type, file.headers)}).content
+
+        return end_screen
+        return "upload button pressed"
+
+@app.route('/notif', methods=['GET'])
+def notif():
+    return "Notifications will be done one day..... hopefully!!!"
 
 
 if __name__ == '__main__':
