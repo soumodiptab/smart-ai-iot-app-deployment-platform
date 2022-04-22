e8ccfed77e68eeeb9c28dfd6759d6c7fd8bc34b0
akileshpanicker <48186557+akileshpanicker@users.noreply.github.com>
2022-04-22 10:37:52 +0530

    Delete packaging_details directory

--- packaging_details/sample_app/model_out_test.py
+++
@@ -1,3 +0,0 @@
-import requests
-sample={'temp_max': 371, 'perc': 217}
-json_out = requests.post(prediction_api, json=sample).json()
--- packaging_details/sample_app_1/dataScFiles/artifacts/postprocess.py
+++
@@ -1,4 +0,0 @@
-def postprocess(l):
-    l=l[0]
-    d = {0:'Drizzle', 1:'Fog', 2:'Rain', 3:'Snow', 4:'Sun'}
-    return d[l]
