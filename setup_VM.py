9f0c3c8911b9e9a7cabe21bf69e81112b4da3e64
sharma18yash <sharma18yash.ys@gmail.com>
2022-05-01 15:25:24 +0530

    installing flask

--- setup_VM.py
+++ setup_VM.py
@@ -34,6 +34,7 @@ def start_SA(ip):
         "pip install pymongo",
         "pip install kafka-python",
         "pip install dnspython",
+        "pip install flask",
         "sudo chmod 777 /var/run/docker.sock",
         "python3 git_clone_start_SA.py &",
         "logout",
