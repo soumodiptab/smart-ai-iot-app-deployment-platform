87d44bfaab44efb044e24c0b3c067f3c39f2f245
mukundamMayank <mayank.mukundam@students.iiit.ac.in>
2022-05-02 20:25:27 +0530

    deployment chnages updated

--- node_manager/producer.py
+++ node_manager/producer.py
@@ -8,6 +8,6 @@ deploy_producer = KafkaProducer(bootstrap_servers=["52.172.25.250:9092"],
 
 
 req = {"message_type": "SCHED_APP", "app_id": "af1aefd3878148e0922ef8c78f364b80", "isModel": True, "app_instance_id": "af1aefd3878148e0922ef8c78f364b80",
-       "start_time": "17:53", "end_time": "05:35", "periodicity": "5", "burst_time": "1", "periodicity_unit": "Hrs"}
+       "start_time": "19:56", "end_time": "05:35", "periodicity": "5", "burst_time": "1", "periodicity_unit": "Hrs"}
 deploy_producer.send("scheduler", req)
 time.sleep(2)
