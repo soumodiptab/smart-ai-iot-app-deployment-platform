2adf5ea0679a9456fcb7dbc581c6f4e6569c68f0
Soumodipta Bose <sbose2019@gmail.com>
2022-05-04 11:17:54 +0530

    bye for this sem

--- service_agent/service_lifecycle_sim.py
+++ service_agent/service_lifecycle_sim.py
@@ -6,7 +6,7 @@ KAFKA_SERVERS = json_config_loader('config/kafka.json')["bootstrap_servers"]
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVERS,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 # service_ agent topic : service_127.0.0.1
-producer.send("service_157.48.136.78",
+producer.send("service_20.207.107.115",
               {
                   "command": "START",
                   "service": "ai_manager"
